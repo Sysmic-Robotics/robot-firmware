4c6bcb9b6517072bd099b9f6f074ddb778307d83
Pablo Reyes Robles <pabloreyes500@gmail.com>
2019-06-18 01:10:05 -0400

    I2C transmit and receive functions added

--- Hal/sysmic_ll_i2c.c
+++ Hal/sysmic_ll_i2c.c
@@ -1,20 +1,166 @@
-#include "sysmic_ll_spi.h"
+#include "sysmic_ll_i2c.h"
 
-uint8_t I2C_Write(I2C_TypeDef *I2Cx, uint8_t txData)
+void I2C_Write(I2C_TypeDef *I2Cx, uint8_t address, uint8_t txData)
 {
     LL_I2C_GenerateStartCondition(I2Cx);
 
     while (!LL_I2C_IsActiveFlag_BUSY(I2Cx));
     
-    
+    LL_I2C_DisableBitPOS(I2Cx);
+
+    while(!LL_I2C_IsActiveFlag_SB(I2Cx));
+    /* TODO: check address shift */
+    LL_I2C_TransmitData8(I2Cx, address & I2C_WRITE_OPERATION);
+
+    while(!LL_I2C_IsActiveFlag_ADDR(I2Cx));
+
+    LL_I2C_ClearFlag_ADDR(I2Cx);
+
+    while(!LL_I2C_IsActiveFlag_TXE(I2Cx));
+
+    LL_I2C_TransmitData8(I2Cx, txData);
+
+    while(!LL_I2C_IsActiveFlag_BTF(I2Cx));
+
+    LL_I2C_GenerateStopCondition(I2Cx);
 }
 
-void I2C_WriteMultiple(I2C_TypeDef *I2Cx, uint8_t *txData, uint16_t len)
+void I2C_WriteMultiple(I2C_TypeDef *I2Cx, uint8_t address, uint8_t *txData, uint16_t len)
 {
+    LL_I2C_GenerateStartCondition(I2Cx);
+
+    while (!LL_I2C_IsActiveFlag_BUSY(I2Cx));
     
+    LL_I2C_DisableBitPOS(I2Cx);
+
+    while(!LL_I2C_IsActiveFlag_SB(I2Cx));
+    /* TODO: check address shift */
+    LL_I2C_TransmitData8(I2Cx, address & I2C_WRITE_OPERATION);
+
+    while(!LL_I2C_IsActiveFlag_ADDR(I2Cx));
+
+    LL_I2C_ClearFlag_ADDR(I2Cx);
+
+    while (len)
+    {
+        while(!LL_I2C_IsActiveFlag_TXE(I2Cx));
+
+        LL_I2C_TransmitData8(I2Cx, txData);
+        len--;
+
+        if((LL_I2C_IsActiveFlag_TXE(I2Cx) && LL_I2C_IsActiveFlag_BTF(I2Cx)) && len > 0)
+        {
+            LL_I2C_TransmitData8(I2Cx, txData);
+            len--;
+        }
+
+        while(!LL_I2C_IsActiveFlag_BTF(I2Cx));
+    }
+
+    LL_I2C_GenerateStopCondition(I2Cx);
 }
 
-void I2C_ReadMultiple(I2C_TypeDef *I2Cx, uint8_t *rxData, uint8_t dummyData, uint16_t len)
+uint8_t I2C_Read(I2C_TypeDef *I2Cx, uint8_t address)
 {
+    uint8_t rxData;
+
+    LL_I2C_GenerateStartCondition(I2Cx);
+
+    while (!LL_I2C_IsActiveFlag_BUSY(I2Cx));
     
+    LL_I2C_DisableBitPOS(I2Cx);
+
+    while(!LL_I2C_IsActiveFlag_SB(I2Cx));
+    /* TODO: check address shift */
+    LL_I2C_TransmitData8(I2Cx, address & I2C_READ_OPERATION);
+
+    while(!LL_I2C_IsActiveFlag_ADDR(I2Cx));
+
+    LL_I2C_AcknowledgeNextData(I2Cx, LL_I2C_NACK);
+    LL_I2C_GenerateStopCondition(I2Cx);
+    LL_I2C_ClearFlag_ADDR(I2Cx);
+
+    while(!LL_I2C_IsActiveFlag_RXNE(I2Cx));
+
+    rxData = LL_I2C_ReceiveData8(I2Cx);
+
+    while(!LL_I2C_IsActiveFlag_BTF(I2Cx));
+
+    return rxData;
+}
+
+void I2C_ReadMultiple(I2C_TypeDef *I2Cx, uint8_t address, uint8_t *rxData, uint16_t len)
+{
+    LL_I2C_GenerateStartCondition(I2Cx);
+
+    while (!LL_I2C_IsActiveFlag_BUSY(I2Cx));
+    
+    LL_I2C_DisableBitPOS(I2Cx);
+
+    while(!LL_I2C_IsActiveFlag_SB(I2Cx));
+    /* TODO: check address shift */
+    LL_I2C_TransmitData8(I2Cx, address & I2C_READ_OPERATION);
+
+    while(!LL_I2C_IsActiveFlag_ADDR(I2Cx));
+
+    if(len == 2)
+    {
+        LL_I2C_AcknowledgeNextData(I2Cx, LL_I2C_NACK);
+        LL_I2C_ClearFlag_ADDR(I2Cx);
+        LL_I2C_EnableBitPOS(I2Cx);
+    }
+    else
+    {
+        LL_I2C_AcknowledgeNextData(I2Cx, LL_I2C_ACK);
+        LL_I2C_ClearFlag_ADDR(I2Cx);
+    }
+
+    while (len)
+    {
+        if(len == 2)
+        {
+            while(!LL_I2C_IsActiveFlag_BTF(I2Cx));
+
+            rxData++ = LL_I2C_ReceiveData8(I2Cx);
+            len--;
+
+            LL_I2C_AcknowledgeNextData(I2Cx, LL_I2C_NACK);
+            LL_I2C_GenerateStopCondition(I2Cx);
+
+            rxData++ = LL_I2C_ReceiveData8(I2Cx);
+            len--;
+        }
+        else if(len == 3)
+        {
+            while(!LL_I2C_IsActiveFlag_BTF(I2Cx));
+
+            LL_I2C_AcknowledgeNextData(I2Cx, LL_I2C_NACK);
+            rxData++ = LL_I2C_ReceiveData8(I2Cx);
+            len--;
+
+            while(!LL_I2C_IsActiveFlag_BTF(I2Cx));
+            LL_I2C_GenerateStopCondition(I2Cx);
+
+            rxData++ = LL_I2C_ReceiveData8(I2Cx);
+            len--;
+
+            rxData++ = LL_I2C_ReceiveData8(I2Cx);
+            len--;
+        }
+        else
+        {
+            while(!LL_I2C_IsActiveFlag_RXNE(I2Cx));
+
+            rxData++ = LL_I2C_ReceiveData8(I2Cx);
+            len--;
+
+            if((LL_I2C_IsActiveFlag_RXNE(I2Cx) && LL_I2C_IsActiveFlag_BTF(I2Cx)) && len > 0)
+            {
+                rxData++ = LL_I2C_ReceiveData8(I2Cx);
+                len--;
+            }
+
+            while(!LL_I2C_IsActiveFlag_BTF(I2Cx));
+        }        
+    }
 }
--- Hal/sysmic_ll_i2c.h
+++ Hal/sysmic_ll_i2c.h
@@ -1,25 +1,44 @@
+/**
+ * @author  Pablo Reyes Robles
+ * @email   pablo.reyesr@alumnos.usm.cl
+ * @version v1.0
+ * @license UTFSM
+@verbatim
+   ----------------------------------------------------------------------
+    Copyright (C) Sysmic Robotics, 2019
+   ----------------------------------------------------------------------
+@endverbatim
+ */
+
 #include "stm32f4xx_ll_i2c.h"
 
-/**
- * @brief Write a single byte through I2C
- * @param I2Cx: I2C instance
- * @param txData: byte to send
- */
-uint8_t I2C_Write(I2C_TypeDef *I2Cx, uint8_t txData);
+enum {
+    I2C_WRITE_OPERATION = 0,
+    I2C_READ_OPERATION
+};
 
 /**
- * @brief Write multiple bytes through I2C
- * @param I2Cx: I2C instance
- * @param txData: data buffer to send
- * @param len: data array length
+ * \brief Write a single byte through I2C
+ * \param I2Cx: I2C instance
+ * \param address: device address
+ * \param txData: byte to send
  */
-void I2C_WriteMultiple(I2C_TypeDef *I2Cx, uint8_t *txData, uint16_t len);
+void I2C_Write(I2C_TypeDef *I2Cx, uint8_t address, uint8_t txData);
 
 /**
- * @brief Write multiple bytes through I2C
- * @param I2Cx: I2C instance
- * @param rxData: data buffer to receive data
- * @param dummyData: dummy byte to receive data from device
- * @param len: data array length
+ * \brief Write multiple bytes through I2C
+ * \param I2Cx: I2C instance
+ * \param address: device address
+ * \param txData: data buffer to send
+ * \param len: data array length
  */
-void I2C_ReadMultiple(I2C_TypeDef *I2Cx, uint8_t *rxData, uint8_t dummyData, uint16_t len);
+void I2C_WriteMultiple(I2C_TypeDef *I2Cx, uint8_t address, uint8_t *txData, uint16_t len);
+
+/**
+ * \brief Write multiple bytes through I2C
+ * \param I2Cx: I2C instance
+ * \param address: device address
+ * \param rxData: data buffer to receive data
+ * \param len: data array length
+ */
+void I2C_ReadMultiple(I2C_TypeDef *I2Cx, uint8_t address, uint8_t *rxData, uint16_t len);
