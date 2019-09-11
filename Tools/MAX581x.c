9fc086373d5e169a1e334b2a65daccb2a7bd789e
Pablo Reyes Robles <pabloreyes500@gmail.com>
2019-09-11 18:08:08 -0300

    MAX581x generic lib added

---
+++ Tools/MAX581x.c
@@ -0,0 +1,102 @@
+#include "MAX581x.h"
+
+void MAX581x_WriteCommand(MAX581x_Handler_t *dacDevice)
+{
+	dacDevice->i2cAddress = 0x10 << 1 | MAX581x_ADDRESS | MAX581x_WRITE_OPERATION;
+	HAL_I2C_Master_Transmit(dacDevice->i2cHandler, dacDevice->i2cAddress, dacDevice->txBuffer, 3, 1000);
+}
+
+void MAX581x_ReadCommand(MAX581x_Handler_t *dacDevice)
+{
+	dacDevice->i2cAddress = 0x10 << 1 | MAX581x_ADDRESS | MAX581x_READ_OPERATION;
+	HAL_I2C_Master_Transmit(dacDevice->i2cHandler, dacDevice->i2cAddress, dacDevice->txBuffer, 3, 1000);
+}
+
+void MAX581x_Init(MAX581x_Handler_t *dacDevice, I2C_HandleTypeDef *hi2c, uint8_t dacRefSelector)
+{
+	dacDevice->i2cHandler = hi2c;
+
+	MAX581x_Config(dacDevice, MAX581x_DISABLE_LATCH | MAX581x_SEL_ALL, MAX581x_SEL_A | MAX581x_SEL_B | MAX581x_SEL_C | MAX581x_SEL_D);
+	MAX581x_Reference(dacDevice, dacRefSelector | MAX581x_REF_PWR_ON);
+}
+
+void MAX581x_Config(MAX581x_Handler_t *dacDevice, uint8_t dacLatch, uint8_t dacConfigSelector)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_CONFIG | dacLatch;
+	dacDevice->txBuffer[1] = dacConfigSelector;
+	dacDevice->txBuffer[2] = 0x00;
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_Reference(MAX581x_Handler_t *dacDevice, uint8_t dacRefConfig)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_REF | dacRefConfig;
+	dacDevice->txBuffer[1] = 0x00;
+	dacDevice->txBuffer[2] = 0x00;
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_Code(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_CODEn | dacSelector;
+	dacDevice->txBuffer[1] = (uint8_t)((dacData >> 2) & 0xFF);
+	dacDevice->txBuffer[2] = (uint8_t)((dacData << 6) & 0xF0);
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_Load(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_LOADn | dacSelector;
+	dacDevice->txBuffer[1] = 0x00;
+	dacDevice->txBuffer[2] = 0x00;
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_CodeLoadAll(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_CODEn_LOAD_ALL | dacSelector;
+	dacDevice->txBuffer[1] = (uint8_t)((dacData >> 2) & 0xFF);
+	dacDevice->txBuffer[2] = (uint8_t)((dacData << 6) & 0xF0);
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_CodeLoad(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_CODEn_LOADn | dacSelector;
+	dacDevice->txBuffer[1] = (uint8_t)((dacData >> 2) & 0xFF);
+	dacDevice->txBuffer[2] = (uint8_t)((dacData << 6) & 0xF0);
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_CodeAll(MAX581x_Handler_t *dacDevice, uint16_t dacData)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_CODE_ALL;
+	dacDevice->txBuffer[1] = (uint8_t)((dacData >> 2) & 0xFF);
+	dacDevice->txBuffer[2] = (uint8_t)((dacData << 6) & 0xF0);
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_LoadAll(MAX581x_Handler_t *dacDevice)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_LOAD_ALL;
+	dacDevice->txBuffer[1] = 0x00;
+	dacDevice->txBuffer[2] = 0x00;
+
+	MAX581x_WriteCommand(dacDevice);
+}
+
+void MAX581x_CodeAllLoadAll(MAX581x_Handler_t *dacDevice, uint16_t dacData)
+{
+	dacDevice->txBuffer[0] = MAX581x_CMD_CODE_ALL_LOAD_ALL;
+	dacDevice->txBuffer[1] = (uint8_t)((dacData >> 2) & 0xFF);
+	dacDevice->txBuffer[2] = (uint8_t)((dacData << 6) & 0xF0);
+
+	MAX581x_WriteCommand(dacDevice);
+}
---
+++ Tools/MAX581x.h
@@ -0,0 +1,119 @@
+/**
+ * @author  Pablo Reyes Robles
+ * @email   pablo.reyesr@alumnos.usm.cl
+ * @version v1.0
+ * @license UTFSM
+@verbatim
+   ----------------------------------------------------------------------
+    Copyright (C) Sysmic Robotics, 2019
+   ----------------------------------------------------------------------
+@endverbatim
+ */
+
+#ifndef _MAX581x_H_
+#define _MAX581x_H_
+
+/* Include proper header file */
+/* STM32F7xx */
+#if defined(STM32F0xx) || defined(STM32F0XX)
+#ifndef STM32F0xx
+#define STM32F0xx
+#endif
+#ifndef STM32F0XX
+#define STM32F0XX
+#endif
+#include "stm32f0xx.h"
+#include "stm32f0xx_hal.h"
+#endif
+
+/* STM32F4xx */
+#if defined(STM32F4xx) || defined(STM32F4XX) || defined(STM32F4)
+#ifndef STM32F4xx
+#define STM32F4xx
+#endif
+#ifndef STM32F4XX
+#define STM32F4XX
+#endif
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+#endif
+
+/* STM32F7xx */
+#if defined(STM32F7xx) || defined(STM32F7XX)
+#ifndef STM32F7xx
+#define STM32F7xx
+#endif
+#ifndef STM32F7XX
+#define STM32F7XX
+#endif
+#include "stm32f7xx.h"
+#include "stm32f7xx_hal.h"
+#endif
+
+#include "stm32f4xx_hal.h"
+
+#define MAX581x_ADDRESS                 0x00
+#define MAX581x_WRITE_OPERATION         0x00
+#define MAX581x_READ_OPERATION          0x01
+
+#define MAX581x_CMD_CODEn               0x00
+#define MAX581x_CMD_LOADn               0x10
+#define MAX581x_CMD_CODEn_LOAD_ALL      0x20
+#define MAX581x_CMD_CODEn_LOADn         0x30
+#define MAX581x_CMD_CODE_ALL            0x80
+#define MAX581x_CMD_LOAD_ALL            0x81
+#define MAX581x_CMD_CODE_ALL_LOAD_ALL   0x82
+
+#define MAX581x_CMD_POWER               0x40
+#define MAX581x_CMD_SW_CLEAR			0x50
+#define MAX581x_CMD_SW_RESET			0x51
+#define MAX581x_CMD_CONFIG				0x60
+#define MAX581x_CMD_REF					0x70
+
+#define MAX581x_REF_EXT					0x00
+#define MAX581x_REF_25					0x01
+#define MAX581x_REF_20					0x02
+#define MAX581x_REF_41					0x03
+#define MAX581x_REF_PWR_ON				0x04
+
+#define MAX581x_OUTPUT_A				0x00
+#define MAX581x_OUTPUT_B				0x01
+#define MAX581x_OUTPUT_C				0x02
+#define MAX581x_OUTPUT_D				0x03
+#define MAX581x_OUTPUT_ALL				0x04
+
+#define MAX581x_ENABLE_LATCH			0x00
+#define MAX581x_DISABLE_LATCH			0x01
+#define MAX581x_SEL_ALL					0x40
+#define MAX581x_SEL_A					0x01
+#define MAX581x_SEL_B					0x02
+#define MAX581x_SEL_C					0x04
+#define MAX581x_SEL_D					0x08
+
+#ifdef	MAX5814
+#define MAX581x_MAX_VAL					1023
+#elif	MAX5815
+#define MAX581x_MAX_VAL					4095
+#endif
+
+typedef struct MAX581x {
+	I2C_HandleTypeDef *i2cHandler;
+	uint8_t i2cAddress;
+	uint8_t txBuffer[3];
+	uint8_t rxBuffer[3];
+} MAX581x_Handler_t;
+
+void MAX581x_WriteCommand(MAX581x_Handler_t *dacDevice);
+void MAX581x_ReadCommand(MAX581x_Handler_t *dacDevice);
+void MAX581x_Init(MAX581x_Handler_t *dacDevice, I2C_HandleTypeDef *hi2c, uint8_t dacRefSelector);
+void MAX581x_Config(MAX581x_Handler_t *dacDevice, uint8_t dacLatch, uint8_t dacConfigSelector);
+void MAX581x_Reference(MAX581x_Handler_t *dacDevice, uint8_t dacRefConfig);
+void MAX581x_Code(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData);
+void MAX581x_Load(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData);
+void MAX581x_CodeLoadAll(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData);
+void MAX581x_CodeLoad(MAX581x_Handler_t *dacDevice, uint8_t dacSelector, uint16_t dacData);
+void MAX581x_CodeAll(MAX581x_Handler_t *dacDevice, uint16_t dacData);
+void MAX581x_LoadAll(MAX581x_Handler_t *dacDevice);
+void MAX581x_CodeAllLoadAll(MAX581x_Handler_t *dacDevice, uint16_t dacData);
+
+#endif
